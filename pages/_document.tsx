24c5dd26a7c2379ab2b3d39b39ac06648a05e1f4
Dev <Dev@kakis-iMac-Pro.local>
2024-01-08 18:20:55 -0300

    feat: initial commit - prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='w-[100vw] h-[100vh] bg-zinc-600'>
         <Main />
         <NextScript />
       </body>
